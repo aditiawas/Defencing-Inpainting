aa026ca8a3e704da9b6d3e7056b1ae4786288619
simonkayar <37694037+simonkayar@users.noreply.github.com>
2020-03-25 19:26:57 +0530

    Add files via upload
    Adding code that makes the OR operation better by having limits within the various horizontal, vertical and diagonal components. Also added some titles to images.

--- phase 2.py
+++ phase 2.py
@@ -14,17 +14,43 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imwrite('gray_image.png',gray_image)
 x=np.asarray(Image.open("gray_image.png").convert("L"))
 wp = pywt.WaveletPacket2D(data=x, wavelet='db1', mode='sym')
+plt.title("Original Image")
 plt.imshow(wp.data,plt.cm.gray) # plot original image
 plt.show()
-z=wp['h'].data
-z[z<0.0]=0.0
-plt.imshow(z,plt.cm.gray) # plot horizontal decomposition of image
+
+limith = 25
+limitv = 20
+limitd = 20
+
+zh=wp['h'].data
+zh[zh<limith]=0.0
+plt.title("Horizontal")
+plt.imshow(zh,plt.cm.gray) # plot horizontal decomposition of image
 plt.show()
-z=wp['v'].data
-z[z<0.0]=0.0
-plt.imshow(z,plt.cm.gray) # plot vertical decomposition of image
+
+
+zv=wp['v'].data
+zv[zv<limitv]=0.0
+plt.title("Vertical")
+plt.imshow(zv,plt.cm.gray) # plot vertical decomposition of image
 plt.show()
-z=wp['d'].data
-z[z<0.0]=0.0
-plt.imshow(z,plt.cm.gray) # plot diagonal decmposition of image
+
+zd=wp['d'].data
+zd[zd<limitd]=0.0
+plt.title("Diagonal")
+plt.imshow(zd,plt.cm.gray) # plot diagonal decmposition of image
 plt.show()
+
+
+zf = cv2.bitwise_or(zh, zd)
+zf = cv2.bitwise_or(zf, zv)
+cv2.imshow('Bitwise OR', zf)
+#
+## De-allocate any associated memory usage   
+if cv2.waitKey(0) & 0xff == 27:  
+    cv2.destroyAllWindows()
+
+
+#zf[zf<0.0]=0.0
+#plt.imshow(zf,plt.cm.gray) # plot final decmposition of image
+#plt.show()
